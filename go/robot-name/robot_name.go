73e1ce4a0c6e6d9c652bec858814d33db1c1ba1f
Bhakiyaraj Kalimuthu <bhakiyaraj.kalimuthu@arangodb.com>
2021-12-04 15:46:41 +0100

    update robot-name exercise

--- go/robot-name/robot_name.go
+++ go/robot-name/robot_name.go
@@ -13,14 +13,23 @@ const (
 // Define the Robot type here.
 type Robot struct {
 	name string
+
 }
+type nameMap map[string]struct{}
 
 var (
 	maxNumberOfRobot = 26*26*10*10*10
-	robotNameList = make(map[string]struct{}, maxNumberOfRobot)
+	robotNameList nameMap
 )
 
+func init(){
+	robotNameList = make(map[string]struct{},maxNumberOfRobot)
+}
+
 func (r *Robot) Name() (string, error) {
+	if r.name != ""{
+		return r.name,nil
+	}
 	if len(robotNameList) == maxNumberOfRobot {
 		return "", fmt.Errorf("space constraint, no more robots can be created")
 	}
@@ -31,10 +40,12 @@ func (r *Robot) Name() (string, error) {
 	}
 	partOne := string(b)
 	partTwo := fmt.Sprintf("%d",100 + rand.Intn(899))
-	r.name = partOne + partTwo
-	if _, ok := robotNameList[r.name]; ok {
-		r.Name()
+	name := partOne + partTwo
+	if _, ok := robotNameList[name]; ok {
+		return r.Name()
 	}
+	r.name = name
+	robotNameList[r.name]= struct{}{}
 	return r.name, nil
 }
 
@@ -42,4 +53,5 @@ func (r *Robot) Reset() {
 	if _,ok := robotNameList[r.name]; ok {
 		delete(robotNameList,r.name)
 	}
+	r.name =""
 }
